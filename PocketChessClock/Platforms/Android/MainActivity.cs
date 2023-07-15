4f915223d33b7eaee9498fe51053d8c675d87e3e
Eugene Levakhin <eugene.levakhin@gmail.com>
2023-07-15 11:14:44 +0300

    fullscreen mode and hide navigation bar, remove Title

--- PocketChessClock/Platforms/Android/MainActivity.cs
+++ PocketChessClock/Platforms/Android/MainActivity.cs
@@ -1,10 +1,18 @@
-﻿using Android.App;
+﻿using Android.OS;
+using Android.App;
+using Android.Views;
 using Android.Content.PM;
-using Android.OS;
 
 namespace PocketChessClock;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        Platform.Init(this, savedInstanceState);
+        this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+    }
 }
